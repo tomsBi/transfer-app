1903bd7ee84a8a5c14190c57ab90d1c8dab5208e
Toms <toms.bi@inbox.lv>
2023-11-21 14:09:55 +0200

    add exceptions

---
+++ app/Exceptions/TransactionException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Exceptions;
+
+
+class TransactionException extends CustomException
+{
+    public static function sameAccountException(): TransactionException
+    {
+        return new self(message: 'Transaction between the same account is not allowed.', code:403);
+    }
+
+    public static function wrongCurrencyException(): TransactionException
+    {
+        return new self(message: 'Wrong currency.', code:400);
+    }
+
+    public static function transactionFailedException(): TransactionException
+    {
+        return new self(message: 'Transaction failed.', code:500);
+    }
+}
